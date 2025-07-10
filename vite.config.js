a721cffbd12c7041ca7d6b169118e887d1cb2173
Karl Rito <karllouiserito08@gmail.com>
2025-07-10 18:35:47 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/demo/', // 👈 Add this line with your repo name
   plugins: [
     vue(),
     vueDevTools(),
