b1c15e7ac98ba15593bd1a8df49dd7e490f49cd3
Karl Rito <karllouiserito08@gmail.com>
2025-07-10 18:54:40 +0800

    i[d

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   },
+  // },
 })
